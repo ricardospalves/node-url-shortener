af4a4e6a7128610b2e92d24227c94cfb530822fa
Ricardo Alves <ricardospalves@gmail.com>
2024-03-24 13:17:09 -0300

    refactor: add /api prefix to routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import fastify from 'fastify'
+import fastify, { RegisterOptions } from 'fastify'
 import { createURL, listURLs } from './routes/url'
+
+const FASTIFY_REGISTER_OPTIONS: RegisterOptions = {
+  prefix: '/api',
+}
 
 const app = fastify()
 
-app.register(createURL)
-app.register(listURLs)
+app.register(createURL, FASTIFY_REGISTER_OPTIONS)
+app.register(listURLs, FASTIFY_REGISTER_OPTIONS)
 
 export { app }
