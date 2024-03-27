50257065b89eb8ccd0459b30c5bfb05b8fee769a
Ricardo Alves <ricardospalves@gmail.com>
2024-03-27 00:18:25 -0300

    refactor: create code via app

--- api/scripts.sh
+++ api/scripts.sh
@@ -1,7 +1,7 @@
 echo 'Register a short URL for Node.js:\n'
 curl --silent -X POST \
   -H 'Content-Type: application/json' \
-  --data-binary '{"code":"nodejs","url":"https://nodejs.org"}' \
+  --data-binary '{"url":"https://nodejs.org"}' \
   localhost:3333/api/short-urls
 
 echo '\n\n********\n'
@@ -9,7 +9,7 @@ echo '\n\n********\n'
 echo 'Register a short URL for GitHub:\n'
 curl --silent -X POST \
   -H 'Content-Type: application/json' \
-  --data-binary '{"code":"github","url":"https://github.com"}' \
+  --data-binary '{"url":"https://github.com"}' \
   localhost:3333/api/short-urls
 
 echo '\n\n********\n'
@@ -17,7 +17,7 @@ echo '\n\n********\n'
 echo 'Register a short URL for GitHub:\n'
 curl --silent -X POST \
   -H 'Content-Type: application/json' \
-  --data-binary '{"code":"github","url":"https://github.com"}' \
+  --data-binary '{"url":"https://github.com"}' \
   localhost:3333/api/short-urls
 
 echo '\n\n********\n'
@@ -25,7 +25,7 @@ echo '\n\n********\n'
 echo 'Register a short URL for npm:\n'
 curl --silent -X POST \
   -H 'Content-Type: application/json' \
-  --data-binary '{"code":"npm","url":"https://npmjs.com"}' \
+  --data-binary '{"url":"https://npmjs.com"}' \
   localhost:3333/api/short-urls
 
 echo '\n'
--- api/src/routes/url/createURL.route.ts
+++ api/src/routes/url/createURL.route.ts
@@ -2,26 +2,28 @@ import { FastifyInstance } from 'fastify'
 import { z as zod } from 'zod'
 import { sql } from '../../lib/postgres'
 import { PostgresError } from 'postgres'
+import { randomString } from '../../utils/randomString'
 
 const schema = zod.object({
-  code: zod.string().min(3),
   url: zod.string().url(),
 })
 
 export const createURL = async (app: FastifyInstance) => {
   return app.post('/short-url', async (request, reply) => {
-    const { code, url } = schema.parse(request.body)
+    const { url } = schema.parse(request.body)
+    const code = randomString(10)
 
     try {
       const results = await sql`
         INSERT INTO short_urls (code, original_url)
         VALUES (${code}, ${url})
-        RETURNING id
+        RETURNING id, code
       `
       const shortUrl = results[0]
 
       return reply.status(201).send({
-        shortUrlId: shortUrl.id,
+        id: shortUrl.id,
+        shortUrl: `http://localhost:3333/${shortUrl.code}`,
       })
     } catch (error) {
       if (error instanceof PostgresError) {
