d2ed426012687e8a1b1d815baa8b5b6aabeb2949
Ricardo Alves <ricardospalves@gmail.com>
2024-03-29 04:29:56 -0300

    feat(web): add StartIcon prop to Button

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -1,20 +1,31 @@
 import { classNames } from '@/utils/classNames'
 import { ComponentPropsWithoutRef } from 'react'
+import { IconBase } from 'react-icons'
 
 type NativeProps = ComponentPropsWithoutRef<'button'>
 
-export type ButtonProps = NativeProps
+export type ButtonProps = NativeProps & {
+  StartIcon?: typeof IconBase
+}
 
-export const Button = ({ children, className, ...restProps }: ButtonProps) => {
+export const Button = ({
+  children,
+  className,
+  StartIcon,
+  ...restProps
+}: ButtonProps) => {
   return (
     <button
       className={classNames([
-        'block w-full px-4 h-12 rounded transition-colors font-bold text-center text-white bg-neutral-800 hover:bg-black focus-visible:bg-black focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus:ring-2 focus:ring-orange-500',
+        'flex items-center justify-center gap-2 w-full px-4 h-12 rounded transition-colors font-bold text-center text-white bg-neutral-800 hover:bg-black focus-visible:bg-black focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus:ring-2 focus:ring-orange-500',
         className,
       ])}
       {...restProps}
     >
-      {children}
+      {StartIcon && (
+        <StartIcon className="block shrink-0 size-6" aria-hidden={true} />
+      )}
+      <span className="block shrink-0">{children}</span>
     </button>
   )
 }
