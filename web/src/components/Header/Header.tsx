24757c4591dbbf67a826e05f909d26356beb322d
Ricardo Alves <ricardospalves@gmail.com>
2024-03-29 02:01:26 -0300

    chore: add Header

---
+++ web/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="px-2 py-4 shrink-0">
+      <h1 className="font-bold text-center text-3xl">URL Shortener</h1>
+    </header>
+  )
+}
