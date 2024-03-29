d33bf9c3a1c2217e53125ca948058adc9c93fb1e
Ricardo Alves <ricardospalves@gmail.com>
2024-03-29 03:07:07 -0300

    feat(web): add Input component

---
+++ web/src/components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import { classNames } from '@/utils/classNames'
+import { InputHTMLAttributes, forwardRef } from 'react'
+
+type NativeProps = InputHTMLAttributes<HTMLInputElement>
+
+export type InputProps = NativeProps
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, readOnly, ...restProps }, ref) => {
+    return (
+      <input
+        type="text"
+        className={classNames([
+          'block w-full h-12 px-2 border-2 border-neutral-800 hover:border-black focus-visible:border-black rounded focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus:ring-2 focus:ring-orange-500',
+          readOnly && 'bg-gray-200',
+          className,
+        ])}
+        ref={ref}
+        {...restProps}
+      />
+    )
+  },
+)
+
+Input.displayName = 'Input'
