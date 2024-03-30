010f2b7879b63825d8faa41ba3989cbb64265b1c
Ricardo Alves <ricardospalves@gmail.com>
2024-03-30 02:24:45 -0300

    feat(web): add error props to Input

--- web/src/components/Input/Input.tsx
+++ web/src/components/Input/Input.tsx
@@ -3,16 +3,21 @@ import { InputHTMLAttributes, forwardRef } from 'react'
 
 type NativeProps = InputHTMLAttributes<HTMLInputElement>
 
-export type InputProps = NativeProps
+export type InputProps = NativeProps & {
+  error?: boolean
+}
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, readOnly, ...restProps }, ref) => {
+  ({ className, readOnly, error, ...restProps }, ref) => {
     return (
       <input
         type="text"
         className={classNames([
-          'block w-full h-12 px-2 border-2 border-neutral-800 hover:border-black focus-visible:border-black rounded focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus:ring-2 focus:ring-orange-500',
+          'block w-full h-12 px-2 border-2 rounded focus-visible:outline-none focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 focus:ring-2 focus:ring-orange-500',
           readOnly && 'bg-gray-200',
+          error
+            ? 'border-red-700 bg-red-700/10'
+            : 'border-neutral-800 hover:border-black focus-visible:border-black',
           className,
         ])}
         readOnly={readOnly}
