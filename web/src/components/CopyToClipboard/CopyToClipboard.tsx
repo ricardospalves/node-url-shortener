897b235ed233b378c40453df7e2960741221fa5a
Ricardo Alves <ricardospalves@gmail.com>
2024-04-10 00:36:31 -0300

    feat(web): add CopyToClipboard component

---
+++ web/src/components/CopyToClipboard/CopyToClipboard.tsx
@@ -0,0 +1,56 @@
+import { useShortURLStore } from '@/hooks/useShortURL'
+import { useId, useRef, useState } from 'react'
+import { Input } from '../Input'
+import { Button } from '../Button'
+import { MdCopyAll as CopyIcon } from 'react-icons/md'
+
+export type CopyToClipboardProps = {
+  value: string
+}
+
+export const CopyToClipboard = ({ value }: CopyToClipboardProps) => {
+  const { shortURL } = useShortURLStore((state) => state)
+  const fieldId = useId()
+  const fieldRef = useRef<HTMLInputElement>(null)
+  const [copied, setCopied] = useState(false)
+
+  return (
+    <div>
+      <label htmlFor={fieldId} className="block">
+        Short URL
+      </label>
+
+      <div className="grid sm:grid-cols-12 gap-2">
+        <Input
+          type="text"
+          className="sm:col-span-9 border-dashed"
+          id={fieldId}
+          ref={fieldRef}
+          value={shortURL}
+          readOnly
+        />
+
+        <Button
+          type="submit"
+          className="sm:col-span-3"
+          StartIcon={copied ? undefined : CopyIcon}
+          onClick={() => {
+            ;(async () => {
+              try {
+                await navigator.clipboard.writeText(value)
+
+                fieldRef.current?.select()
+
+                setCopied(true)
+              } catch (error) {
+                console.error('Failed to copy: ', error)
+              }
+            })()
+          }}
+        >
+          {copied ? 'Copied' : 'Copy'}
+        </Button>
+      </div>
+    </div>
+  )
+}
--- web/src/pages/ShortURL/ShortURL.tsx
+++ web/src/pages/ShortURL/ShortURL.tsx
@@ -1,18 +1,11 @@
-import { Button } from '@/components/Button'
-import { Input } from '@/components/Input'
+import { CopyToClipboard } from '@/components/CopyToClipboard/CopyToClipboard'
 import { useShortURLStore } from '@/hooks/useShortURL'
-import { useEffect, useId, useRef, useState } from 'react'
-import {
-  MdCopyAll as CopyIcon,
-  MdChevronLeft as ChevronLeftIcon,
-} from 'react-icons/md'
+import { useEffect } from 'react'
+import { MdChevronLeft as ChevronLeftIcon } from 'react-icons/md'
 import { Link, useNavigate } from 'react-router-dom'
 
 export const ShortURL = () => {
   const { shortURL } = useShortURLStore((state) => state)
-  const fieldId = useId()
-  const fieldRef = useRef<HTMLInputElement>(null)
-  const [copied, setCopied] = useState(false)
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -24,43 +17,7 @@ export const ShortURL = () => {
   return (
     <div className="grow flex items-center justify-center">
       <div className="max-w-lg w-full m-auto">
-        <label htmlFor={fieldId} className="block">
-          Short URL
-        </label>
-
-        <div className="grid sm:grid-cols-12 gap-2">
-          <Input
-            type="text"
-            className="sm:col-span-9 border-dashed"
-            id={fieldId}
-            ref={fieldRef}
-            value={shortURL}
-            readOnly
-          />
-
-          <Button
-            type="submit"
-            className="sm:col-span-3"
-            StartIcon={copied ? undefined : CopyIcon}
-            onClick={() => {
-              ;(async () => {
-                try {
-                  await navigator.clipboard.writeText(
-                    fieldRef.current?.value || '',
-                  )
-
-                  fieldRef.current?.select()
-
-                  setCopied(true)
-                } catch (error) {
-                  console.error('Failed to copy: ', error)
-                }
-              })()
-            }}
-          >
-            {copied ? 'Copied' : 'Copy'}
-          </Button>
-        </div>
+        <CopyToClipboard value={shortURL} />
 
         <p className="mt-8">
           <Link
