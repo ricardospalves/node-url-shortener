9327574423dce74cc0b18f5c104a041fd4adf334
Ricardo Alves <ricardospalves@gmail.com>
2024-04-11 00:43:17 -0300

    refactor(api): improve /short-url GET endpoint

--- api/src/routes/url/listURLs.route.ts
+++ api/src/routes/url/listURLs.route.ts
@@ -1,14 +1,29 @@
 import { FastifyInstance } from 'fastify'
 import { sql } from '../../lib/postgres'
+import { createShortURL } from '../../utils/createShortURL'
+
+type ShortURL = {
+  id: number
+  code: string
+  original_url: string
+  created_at: string
+}
 
 export const listURLs = async (app: FastifyInstance) => {
-  return app.get('/short-url', async () => {
-    const results = await sql`
+  return app.get('/short-url', async (request, reply) => {
+    const results = await sql<ShortURL[]>`
       SELECT id, code, original_url, created_at
       FROM short_urls
       ORDER BY created_at DESC
     `
+    const response = results.map(({ code, id, original_url }) => {
+      return {
+        id: id,
+        shortURL: createShortURL(request, code),
+        originalURL: original_url,
+      }
+    })
 
-    return results
+    return reply.status(200).send(response)
   })
 }
