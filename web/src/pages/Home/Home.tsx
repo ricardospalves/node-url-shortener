24a0e3aaff10324a2859085e68b27ab367f29c7c
Ricardo Alves <ricardospalves@gmail.com>
2024-04-12 00:54:37 -0300

    chore(web): change horizontal padding of RootLayout

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import { Form } from './Form'
 
 export const Home = () => {
   return (
-    <div className="grow flex items-center justify-center">
+    <div className="grow flex items-center justify-center px-4">
       <Form />
     </div>
   )
